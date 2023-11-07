dd34127e548b6437743a6b5b0594ecb3ae74c96c
JulianV122 <julivelo69@gmail.com>
2023-11-06 20:19:08 -0500

    Añadida funcion suscribirse desde frontend y todas politicas

--- Frontend/src/components/enginemasters/enginemasters.js
+++ Frontend/src/components/enginemasters/enginemasters.js
@@ -13,47 +13,47 @@ import {
 } from 'react-router-dom';
 import LoginUser from '../login/LoginUser';
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-    display: "flex",
-    gap: "15px",
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  gap: "15px",
 };
 
 
 export const EngineMasters = () => {
 
 
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const[ logIn, setLogIn] = useState(false)
-    
-    const handleDrawerOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleDrawerClose = () => {
-      setOpen(false);
-    };
+  const [logIn, setLogIn] = useState(false)
 
-    const logout =()=>{
-      setLogIn(false)
-      localStorage.removeItem('token')
-      window.location.reload();
-    }
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-    const token = localStorage.getItem('token')
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <div>
+  const logout = () => {
+    setLogIn(false)
+    localStorage.removeItem('token')
+    window.location.reload();
+  }
 
-        <AppBar position="static" style={{backgroundColor:'#050520'}}>
+  const token = localStorage.getItem('token')
+
+  return (
+    <div>
+
+      <AppBar position="static" style={{ backgroundColor: '#050520' }}>
         <Toolbar style={{ justifyContent: 'space-between' }}>
           <IconButton
             size="large"
@@ -65,66 +65,68 @@ export const EngineMasters = () => {
           >
             <MenuIcon />
           </IconButton>
-         <img className='image-nav' src={images.logo} alt='Engine Masters Logo' />
+          <img className='image-nav' src={images.logo} alt='Engine Masters Logo' />
         </Toolbar>
       </AppBar>
-      
-      <Drawer 
-      anchor="left"
-      open={open}
-      onClose={handleDrawerClose}
-      PaperProps={{
-        sx: {
-          width: 200, // Ajusta el ancho del Drawer según tus necesidades
-          backgroundColor:'#050520',
-          color:'#FFF'
-        },
-      }}
-    >
-  <List sx={{ display: 'flex', flexDirection: 'column', height: '100%', }}>
-  <ListItem >
-  <img className='image-nav' src={images.logo} alt='Engine Masters Logo' />
+
+      <Drawer
+        anchor="left"
+        open={open}
+        onClose={handleDrawerClose}
+        PaperProps={{
+          sx: {
+            width: 200, // Ajusta el ancho del Drawer según tus necesidades
+            backgroundColor: '#050520',
+            color: '#FFF'
+          },
+        }}
+      >
+        <List sx={{ display: 'flex', flexDirection: 'column', height: '100%', }}>
+          <ListItem >
+            <img className='image-nav' src={images.logo} alt='Engine Masters Logo' />
           </ListItem>
           <ListItem button onClick={handleDrawerClose}>
             <ListItemText primary="Quiénes somos" />
           </ListItem>
           <ListItem button onClick={handleDrawerClose}>
-           <Link style={{textDecoration:'none', color:'#FFF'}} to="/">
-            <ListItemText primary="Productos"    />
+            <Link style={{ textDecoration: 'none', color: '#FFF' }} to="/">
+              <ListItemText primary="Productos" />
             </Link>
           </ListItem>
           <ListItem button onClick={handleDrawerClose}>
-            <ListItemText primary="Suscribirse" />
+            <Link style={{ textDecoration: 'none', color: '#FFF' }} to="/suscribirse">
+              <ListItemText primary="Suscribirse" />
+            </Link>
           </ListItem>
           <div style={{ flexGrow: 1 }}></div>
           <ListItem button onClick={handleDrawerClose}>
-         
-         {token ? <Link style={{textDecoration:'none', color:'#FFF'}} onClick={logout}>
-            <ListItemText primary="Logout"    />
-            </Link>: <Link style={{textDecoration:'none', color:'#FFF'}} to="/login">
-            <ListItemText primary="Login"    />
-            </Link> }
-          
-        
+
+            {token ? <Link style={{ textDecoration: 'none', color: '#FFF' }} onClick={logout}>
+              <ListItemText primary="Logout" />
+            </Link> : <Link style={{ textDecoration: 'none', color: '#FFF' }} to="/login">
+              <ListItemText primary="Login" />
+            </Link>}
+
+
 
 
           </ListItem>
         </List>
-    </Drawer>
+      </Drawer>
 
     </div>
 
 
-/*         <div className='container'>
-            <div className='menu'>
-      
-                <div className='ButtonContainer'>
+    /*         <div className='container'>
+                <div className='menu'>
           
-                <Button variant="contained" >
-                    Log in
-                </Button>
+                    <div className='ButtonContainer'>
+              
+                    <Button variant="contained" >
+                        Log in
+                    </Button>
+                    </div>
                 </div>
-            </div>
-        </div> */
-    )
+            </div> */
+  )
 }
